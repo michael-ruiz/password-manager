8eef05b4d7fbe8ca82a7510c605617601f328d3c
Michael Ruiz <mruiz6@uwo.ca>
2022-08-01 23:40:49 -0400

    kinda works

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from database import get_users, get_passwords, confirm_user
 
 app = Flask(__name__)
+users = get_users()
+input_user = ''
+input_passw = ''
 
-@app.route('/helll')
+@app.route('/', methods=['POST', 'GET'])
 def home():
     return render_template('index.html')
 
-@app.route('/')
+@app.route('/passwords', methods=['POST', 'GET'])
 def display():
-    return render_template('passwords.html')
+    input_user = request.form['user']
+    input_passw = request.form['passw']
+
+    if (confirm_user(users, input_user, input_passw)):
+        passwords = get_passwords()
+        return render_template('passwords.html', passwords=passwords)
+
+    return 'no work'
 
 if __name__ == '__main__':
     app.run(debug=True)
--- database.py
+++ database.py
@@ -2,8 +2,19 @@ import mysql.connector
 import os
 
 db = mysql.connector.connect(host='localhost', user='root', passwd=os.getenv('DB_PASS'), database='testDB')
-cursor = db.cursor()
+cursor = db.cursor(buffered=True)
 
-cursor.execute('INSERT INTO Passwords (service, username, password) VALUES (%s, %s, %s)', ('service2', 'user2', 'pass2'))
+def get_passwords():
+    cursor.execute('SELECT * from Passwords')
+    return cursor
 
-db.commit()
+def get_users():
+    cursor.execute('SELECT username, password from Users')
+    return cursor
+
+def confirm_user(users, input_user, input_passw):
+    for user in users:
+        if user[0] == input_user and user[1] == input_passw:
+            return True
+
+    return False
